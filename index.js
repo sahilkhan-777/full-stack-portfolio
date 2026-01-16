d194bea6410dba51c0409e96c8008c5124483e7a
Sahil Khan <sahil4t9@gmail.com>
2026-01-16 21:13:50 +0530

    Add all routes

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/projects", (req, res) => {
+    res.render("projects.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 })
